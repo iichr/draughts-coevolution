304523c5d83402c64c43e3e078336a9279ff255a
Chris Iliev <cii549@cs.bham.ac.uk>
2018-01-27 01:17:39 +0000

    tests up to canMoveInto completed

--- test/BoardvectorSpec.hs
+++ test/BoardvectorSpec.hs
@@ -12,30 +12,55 @@ spec = do
             let boardgetSq = V.fromList [fromJust $ getSquare initialBoard (x,y) | x <- [0..7], y <- [0..7]] 
             let rhs = V.concat $ map fromJust [(\ (VectorBoard x) -> x) initialBoard V.!? r | r <- [0..7]]
             boardgetSq `shouldBe` rhs
+        -- TODO add more tests for getting a position from the board
 
--- Test getSquare
--- get all squares in a flat list :: [Square]
--- [getSquare initialBoard (x,y) | x <- [0..7], y <- [0..7]] 
--- LHS: V.fromList [getSquare initialBoard (x,y) | x <- [0..7], y <- [0..7]] 
--- :: V.Vector Square
--- RHS: V.concat[initialBoard V.! r | r <- [0..7]]
--- test LHS and RHS for equality
+    describe "shouldPromote" $ do
+        it "should indicate whether a piece is promoted at the end of the board" $ do
+            [shouldPromote Black (x,7) | x <- [0..7]] `shouldBe` replicate 7 False ++ [True]
+        it "should promote all Black pieces at the 7th row" $ do
+            [shouldPromote Black (7,x) | x <- [0..7]] `shouldBe` replicate 8 True
+        it "should not promote any pieces given a non-existent row number" $ do
+            [shouldPromote White (20,x) | x <- [0..12]] `shouldBe` replicate 13 False
+        it "should promote all White pieces at the 0th row" $ do
+            [shouldPromote White (0,x) | x <- [0..7]] `shouldBe` replicate 8 True
+        it "should not promote White pieces at the 7th row" $ do
+            [shouldPromote White (7,x) | x <- [0..7]] `shouldBe` replicate 8 False
+        it "should not promote Black pieces at the 0th row" $ do
+            [shouldPromote Black (0,x) | x <- [0..7]] `shouldBe` replicate 8 False
+    
+    describe "kingify" $ do
+        it "turns an ordinary Man tile into a King tile" $ do
+            map kingify [Tile Black Man, Tile White Man] `shouldBe` [Tile Black King, Tile White King]
+        it "does not promote empty tiles" $ do
+            kingify Empty `shouldBe` Empty
 
--- extract V.Vector Square (V.Vector Square) from a VectorBoard 
--- (\ (VectorBoard x) -> x) initialBoard
+    describe "whoseTile" $ do
+        context "when given a tile and a player" $ do
+            it "should return true if the tile belongs to the player" $ do
+                whoseTile (Just $ Tile White Man) White `shouldBe` True
+                whoseTile (Just $ Tile White King) White `shouldBe` True
+                whoseTile (Just $ Tile Black King) Black `shouldBe` True
+                whoseTile (Just $ Tile Black Man) Black `shouldBe` True
+            it "should return false if the tile does not belong to the player" $ do
+                whoseTile (Just $ Tile White Man) Black `shouldBe` False
+                whoseTile (Just $ Tile Black King) White `shouldBe` False
+            it "should return false if tile is empty or nothing" $ do
+                whoseTile (Just Empty) Black `shouldBe` False
+                whoseTile (Just Empty) White `shouldBe` False
+                whoseTile Nothing White `shouldBe` False
+                whoseTile Nothing Black `shouldBe` False
+    
+    describe "canMoveInto" $ do
+        it "is false when on or both of the elements of position is/are out of the board" $ do
+            [canMoveInto initialBoard (x,y) | x <- [-1,-2,-3,-15,28734,8,9,13], y <- [1,2,3,4,0,-121,-31,424]]
+            `shouldBe` replicate 64 False
+        it "is false when the position to move into is not empty" $ do
+            let alloccupied = [(y,x)| x<-[0,2,4,6],y <- [1,5,7]] ++ [(z,k) | z <- [0,2,6], k<-[1,3,5,7]]
+            [canMoveInto initialBoard pss | pss <- alloccupied] `shouldBe` replicate 24 False
+        it "is true when the position is within the board and empty" $ do
+            let allunoccupied = [(x,y)| x<-[0,2,6],y <- [0,2,4,6]] ++ [(z,k) | z <- [1,5,7], k<-[1,3,5,7]] ++ [(3,u)|u<-[0..7]] ++ [(4,v)|v<-[0..7]]
+            [canMoveInto initialBoard pss | pss <- allunoccupied] `shouldBe` replicate 40 True
 
--- putStr $ unlines [unwords [show (arr V.! x V.! y) | x <- [0..7], y <- [0..7]]]
-
--- Test shouldPromote
--- [shouldPromote Black (x,7) | x <- [0..7]]
--- [shouldPromote Black (7,x) | x <- [0..7]]
--- [shouldPromote Black (20,x) | x <- [0..7]]
--- [shouldPromote White (8,x) | x <- [0..7]]
--- [shouldPromote White (0,x) | x <- [0..7]]
--- [shouldPromote White (7,x) | x <- [0..7]]
-
--- Test canMoveInto
--- [canMoveInto initialBoard x | x <- [(-1,-1), (1, -1), (7,7), (7,8), (8,7), (0,0), (5,2), (4,7), (2,5), (2,3), (2,4)]]
 
 -- Test getPlayerMoves
 -- getPlayerMoves (GameState initialBoard White)
