7008a9b665f78cc5d1331ae48be6663ae9203299
Chris Iliev <cii549@cs.bham.ac.uk>
2018-03-30 21:55:32 +0100

    Deployment - randomness 0.2 both

--- app/Main.hs
+++ app/Main.hs
@@ -173,8 +173,8 @@ main = do
     print $ "Mutation rate: " ++  show mutationRate
     print $ "Tournament size: " ++  show tournamentSize
     print "Evaluated against how many randomly selected: 15 "
-    print "Randomness limneg': 0 "
-    print "Randomness lim': 0"
+    print "Randomness limneg': 0.2 "
+    print "Randomness lim': 0.2"
 
 
     -- initialPopulation <- getApopulationFromFile "150popFIRST.txt"
